6a3b43c255783abaa2ba5d55fc11d8056dee671a
mellowmorgan <morgan.waites@gmail.com>
2021-10-18 13:41:02 -0700

    Add conversion functions, basic html
    Co-authored-by Ryan Ashby <rjapsu160@gmail.com>

---
+++ index.js
@@ -0,0 +1,38 @@
+function gallonsToLiters(gallons){
+    return gallons*3.785;
+}
+function litersToGallons(liters){
+  return liters*.264;
+}
+function ouncesToCups(ounces){
+  return ounces*.125;
+}
+function cupsToOunces(cups){
+  return cups*8;
+}
+function ouncesToTablespoons(ounces){
+  return ounces*2;
+}
+function tablespoonsToOunces(tablespoons){
+  return tablespoons*.5;
+}
+function ouncesToMilliliters(ounces){
+  return ounces*30;
+}
+function millilitersToOunces(milliliters){
+  return milliliters*.03;
+}
+function millilitersToTeaspoons(milliters){
+    return milliters*.202884;
+}
+function teaspoonsToMilliliters(teaspoons){
+  return teaspoons*4.928;
+}
+function millilitersToCup(milliliters){
+  return milliliters*.0042;
+}
+function cupsToMilliliters(cups){
+  return cups*237;
+}
+
+
